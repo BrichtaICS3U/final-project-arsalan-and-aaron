e53428c29e473c59ee06d73b50daf48aa286f049
ARSALAN KHAN <S338968746@OCDSB>
2018-05-07 09:44:45 -0400

    dasd

--- TargetClass.py
+++ TargetClass.py
@@ -2,20 +2,16 @@ import pygame
 WHITE = (255, 255, 255)
 BLACK =(0,0,0)
 RED =(255, 0, 0)
+
 class Target(pygame.sprite.Sprite):
 
-    def __init__(self, color, width, height, speed):
+    def __init__(self, color, width, height):
 
          super().__init__()
 
          self.image = pygame.Surface([width, height]) 
-         self.image.fill(RED)
-         self.image.set_colorkey(RED)
-
-         self.width = width 
-         self.height = height
-         self.color = color
-         self.speed = speed
+         self.image.fill(WHITE)
+         self.image.set_colorkey(WHITE)
 
          pygame.draw.ellipse(self.image, RED, [200, 200, 50, 50])
 
